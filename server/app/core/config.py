8ca19ee4f9aa35327b9976445674a0b5776768f3
aditya-arcot <dev@aarcot.com>
2025-12-12 18:33:01 -0600

    add postgres host env var & create db url property

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -1,7 +1,9 @@
+from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    POSTGRES_HOST: str
     POSTGRES_PORT: int
     POSTGRES_DB: str
     POSTGRES_USER: str
@@ -11,6 +13,15 @@ class Settings(BaseSettings):
         env_file="../.env",
         extra="ignore",
     )
+
+    @computed_field
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
 
 
 settings = Settings()  # type: ignore
